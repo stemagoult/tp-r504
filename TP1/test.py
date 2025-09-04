b4086b1d13e9eb6ab6d7e1f97fe020138f4f2a13
stemagoult <samy>
2025-09-04 14:31:38 +0200

    maj

--- TP1/test.py
+++ TP1/test.py
@@ -13,4 +13,10 @@ def test_2():
 	assert f.puissance(-2,-1) == -0.5
 
 def test_3():
-	assert f.puissance(
+	assert f.puissance(0,1) == 0
+	assert f.puissance(0,10) == 0
+	assert f.puissance(0,100) == 0
+	assert f.puissance(0,1000) == 0
+
+def test_4():
+	assert f puissance(
